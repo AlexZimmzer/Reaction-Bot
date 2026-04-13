5dd6d100af506a876747e7a557784b873ce77c80
Alex Zimmer <164894355+AlexZimmzer@users.noreply.github.com>
2026-04-12 22:30:25 -0400

    Use environment variable for Discord token

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import asyncio
 from datetime import datetime, timedelta
 
 # Token for bot connection
-TOKEN = "
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 heartList = ["❤️", "🤎", "🧡", "💛", "💚", "💙", "🩵", "💜",
              "🩷", "🖤", "🩶", "🤍", "❣️", "💕", "💞", "💓",
